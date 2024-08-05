e1f631a3f19fed054ca002e55f0027f300dfb495
Akash Patil <109128432+akashpatil108@users.noreply.github.com>
2024-08-05 14:45:54 +0530

    Update stroke_prediction.py

--- stroke_prediction.py
+++ stroke_prediction.py
@@ -10,10 +10,10 @@ with open(filename, "rb") as file:
 # Create a dictionary to map categorical features to their respective mappings
 categorical_mappings = {
     'gender': {'Female': 0, 'Male': 1, 'Other': 3},
-    'work_type': {'Private': 0, 'Self-employed': 1, 'Children': 3, 'Govt_job': 4, 'Never_worked': 5},
+    'work_type': {'Private': 0, 'Self-employed': 1, 'Children': 2, 'Govt_job': 3, 'Never_worked': 4},
     'ever_married': {'Yes': 0, 'No': 1},
     'Residence_type': {'Urban': 0, 'Rural': 1},
-    'smoking_status': {'Never smoked': 0, 'Unknown': 1, 'Formerly smoked': 3, 'Smokes': 4},
+    'smoking_status': {'Never smoked': 0, 'Unknown': 1, 'Formerly smoked': 2, 'Smokes': 3},
     'hypertension': {'No': 0, 'Yes': 1},
     'heart_disease': {'No': 0, 'Yes': 1}
 }
@@ -74,7 +74,7 @@ background_styles = """
 def main():
     # Apply CSS styles
     st.markdown(background_styles, unsafe_allow_html=True)
-    st.markdown('<div style="position: absolute; top: 10px; right: 10px;">Creatd by- Akash Patil</div>', unsafe_allow_html=True)
+    st.markdown('<div style="position: absolute; top: 10px; right: 10px;">Created by- Akash Patil</div>', unsafe_allow_html=True)
 
     st.title("Stroke Prediction")
 
@@ -88,7 +88,6 @@ def main():
     with st.container():
         st.markdown('<div class="container form-section">', unsafe_allow_html=True)
         st.subheader("User Information")
-        # Create input fields for each column
         gender = st.selectbox("Gender", options=["Female", "Male", "Other"], key="gender")
         age = st.number_input("Age", min_value=0, max_value=100, value=30, key="age")
         hypertension = st.selectbox("Hypertension", options=["No", "Yes"], key="hypertension")
@@ -105,23 +104,29 @@ def main():
     with st.container():
         st.markdown('<div class="container">', unsafe_allow_html=True)
         if st.button("Submit", key="prediction_button"):
+            # Create a DataFrame for the input data
             test = pd.DataFrame({'gender': [gender], 'age': [age], 'hypertension': [hypertension],
                                  'heart_disease': [heart_disease], 'ever_married': [ever_married],
                                  'work_type': [work_type], 'Residence_type': [residence_type],
                                  'avg_glucose_level': [avg_glucose_level], 'bmi': [bmi],
                                  'smoking_status': [smoking_status]})
-
+            
             # Map categorical features to their encoded values
             for column, mapping in categorical_mappings.items():
-                test[column] = test[column].map(mapping)
-
-            # Make prediction using the loaded pipeline
-            prediction = loaded_pipeline.predict(test)
-
-            # Show the prediction
-            st.markdown('<div class="prediction">Prediction: Im sorry, You may have a stroke.</div>' if prediction[0] == 1 else '<div class="prediction">Prediction:  You may not have a stroke.</div>', unsafe_allow_html=True)
+                if column in test.columns:
+                    test[column] = test[column].map(mapping)
+            
+            # Check for missing values and correct data types
+            if test.isnull().sum().sum() > 0:
+                st.write("Error: Missing values in the input data.")
+            else:
+                try:
+                    # Make prediction using the loaded pipeline
+                    prediction = loaded_pipeline.predict(test)
+                    st.markdown('<div class="prediction">Prediction: I am sorry, you may have a stroke.</div>' if prediction[0] == 1 else '<div class="prediction">Prediction: You may not have a stroke.</div>', unsafe_allow_html=True)
+                except Exception as e:
+                    st.write("Error during prediction:", e)
         st.markdown('</div>', unsafe_allow_html=True)
-
 
 if __name__ == "__main__":
     main()
